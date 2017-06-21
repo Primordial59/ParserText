17e913c4834ef8ab24aa241d6573b8de004808e3
Primordial59 <primordial@perm.ru>
2017-06-21 18:56:35 +0500

    Произведено определение номеров телефонов в выборке

--- Парсер2/Program.cs
+++ Парсер2/Program.cs
@@ -17,10 +17,12 @@ namespace Парсер2
 
             foreach (var b in Block.Load("test.txt"))
             {
-                var str = b.Title + "\n\t" + string.Join("\n\t", b.Body);
+                //                var str = b.Title + "\n\t" + string.Join("\n\t", b.Body);
+                //               var str = b.Title + "\n\t" + string.Join("\n\t", b.PhoneNumber);
+                var str =b.PhoneNumber;
                 System.Diagnostics.Trace.WriteLine(str);
                 Console.WriteLine(str);
-                Console.WriteLine("тест");
+               
 
             }
 
@@ -31,29 +33,43 @@ class Block
         {
             public string Title;
             public IList<string> Body;
+            public string PhoneNumber;
 
-            public static IEnumerable<Block> Load(string path)
+
+
+        public static IEnumerable<Block> Load(string path)
             {
+            String CurrentPhone = "";
+
                 Block ret = null;
-                foreach (var line in File.ReadLines(path).Select(l => l.Trim()))
+                foreach (var line in File.ReadLines(path,Encoding.GetEncoding("windows-1251")).Select(l => l.Trim()))
                 {
-                    if (line.Length == 0 && ret != null)
-                    {
-                        yield return ret;
+                //    if (line.Length == 0 && ret != null)
+                if (ret != null)
+                {
+                    yield return ret;
                         ret = null;
                         continue;
                     }
 
-                    if (line.EndsWith(":"))
+                 //   if (line.EndsWith(":"))
+                    if (line.StartsWith("Детализация"))
                     {
-                        ret = new Block { Title = line.TrimEnd(':'), Body = new List<string>() };
+                    string Phone=line.Substring(53,11);
+                    if (Phone != CurrentPhone)
+                        {
+                        ret = new Block { Title = line.TrimEnd(':'), Body = new List<string>(), PhoneNumber = Phone };
+                        CurrentPhone = Phone;
                         continue;
+                        }
                     }
 
                     if (ret != null)
                         ret.Body.Add(line);
-                }
+                    
+
             }
+        }
         }
 
 
